fa21b446e43bd95dad4e76e3af005b7449e90b37
Rahman-Shaon <rahmanshaon760@gmail.com>
2025-06-27 22:05:13 +0600

    Made choose option

--- Project.c
+++ Project.c
@@ -2,7 +2,13 @@
 
 int main()
 {
+    int choose;
     printf("\t\t\t\tWellcome to Employee management System\n");
+
+    printf("Choose any option\n");
+    printf("1. Admine\n2. Employee\n");
+    scanf("%d", &choose);
+
 
     return 0;
 }
